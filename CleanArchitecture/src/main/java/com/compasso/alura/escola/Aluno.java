6bc71f20b96279cb6257d857aaf8936f39f90fbb
Wesley Schwartz <wesley.schwartz@compasso.com.br>
2023-03-20 17:07:57 -0300

    Criacao do projeto e entity e value objects criados com testes

---
+++ CleanArchitecture/src/main/java/com/compasso/alura/escola/Aluno.java
@@ -0,0 +1,8 @@
+package com.compasso.alura.escola;
+
+public class Aluno {
+    private CPF cpf;
+    private String nome;
+    private Email email;
+
+}
---
+++ CleanArchitecture/src/main/java/com/compasso/alura/escola/CPF.java
@@ -0,0 +1,12 @@
+package com.compasso.alura.escola;
+
+public class CPF {
+    private String cpf;
+
+    public CPF(String cpf) {
+        if (cpf == null || cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("CPF inválido");
+        }
+        this.cpf = cpf;
+    }
+}
